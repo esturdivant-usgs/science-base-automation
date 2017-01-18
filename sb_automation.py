91351b0b0fe65dd200ecd9fae62fbe24a1e688d0
Emily Sturdivant <esturdivant@usgs.gov>
2017-01-18 00:40:10 -0500

    Update autoSB.py
    Add replace_http_in_xml() - not yet functional with fileinput.
    Update exceptions to Python 3.

--- sb_automation.py
+++ sb_automation.py
@@ -31,44 +31,6 @@ from autoSB import *
 from config_autoSB import *
 
 """
-Input variables
-
-# ***REQUIRED***
-# SB username (should be entire USGS email):
-useremail = 'esturdivant@usgs.gov'
-# SB password. If commented out, you will be prompted for your password each time you run the script.
-#password = 
-# URL of data release landing page (e.g. 'https://www.sciencebase.gov/catalog/item/__item_ID__'):
-landing_link = "https://www.sciencebase.gov/catalog/item/58055f50e4b0824b2d1c1ee7"
-# Path to local top directory of data release (equivalent to landing page):
-# If this is a server, it should be mounted and visible in your Volumes. Then it is referenced by '/Volumes/__volume_name__'
-# The volume name should be the name of the directory on the server, rather than the server itself. Check this by looking at the volumes mounted on your computer. In 
-parentdir = r'/Users/esturdivant/Desktop/SE_ATLANTIC'
-# DOI of data release (e.g. '10.5066/F78P5XNK'):
-dr_doi = "10.5066/F74X55X7"
-# Image file (with path) to be used as preview image. If commented out, the preview image will be ignored.
-previewImage = r'/Users/esturdivant/Desktop/SE_ATLANTIC/NASCP_SEAtlantic.png'
-# The edition element in the metadata can be modified here.
-#edition = '1.0'
-
-# ***OPTIONAL***
-# SB fields that will be populated from the XML file in the top directory, assuming an error-free XML is present. Default: citation, body, purpose. Note: body = abstract. Summary will automatically be created from body.
-landing_fields_from_xml = ['citation','body','purpose']
-# SB fields that will be copied (inherited) from the landing page to aggregate pages. Defaults: citation, body (which SB automatically harvests for the summary), purpose.
-subparent_inherits = ['citation','body','purpose']
-# SB fields data pages inherit from their immediate parent page. All other fields will be automatically populated from the XML. Defaults: citation, body.
-data_inherits = ['citation', 'body']
-
-update_landing_page = True # default: True. True if values on the landing page should be updated from XML file in the parent directory
-replace_subpages = False # default: False. True to delete all child pages before running. Not necessary. Use cautiously; deleted items seem to linger in the SB memory so it is best to run the function delete_all_children(sb, landing_id) a few minutes before running the script.
-update_subpages = True # default: True. False to save time if SB pages already exist
-update_XML = True # default: True. False to save time if XML already has the most up-to-date values.
-update_data = True # default: True. False to save time if up-to-date data files have already been uploaded.
-add_preview_image_to_all = False # default: False. True to put the same preview image (file specified above) on every page in the data release.
-
-"""
-#Initialize
-"""
 #%% Initialize SB session
 sb = log_in(useremail)
 """
@@ -83,7 +45,7 @@ print("CITATION: {}".format(landing_item['citation']))
 new_values = {'new_id':landing_item['id'], 'doi':dr_doi, 'landing_link':landing_link}
 
 """
-Work with landing page
+Work with landing page and XML
 """
 # Check for metadata and image files in landing directory
 for f in os.listdir(parentdir):
