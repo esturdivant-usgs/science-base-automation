13e9ff37d427769a1bc0c1140df632bb7d177546
Emily Sturdivant <esturdivant@usgs.gov>
2017-01-20 18:29:05 -0500

    Code to add crossref
    It’s in testing. Still needs to be added to a function.

--- testing.py
+++ testing.py
@@ -13,20 +13,18 @@ from autoSB import *
 from config_autoSB import *
 
 
-"<crossref>
-			<citeinfo>
-				<origin>Robert A. Morton</origin>
-				<origin>Tara L. Miller</origin>
-				<pubdate>2005</pubdate>
-				<title>National Assessment of Shoreline Change: Part 2 Historical Shoreline Changes and Associated Coastal Land Loss along the U.S. Southeast Atlantic Coast</title>
-				<serinfo>
-					<sername>Open-File Report</sername>
-					<issue>2005-1401</issue>
-				</serinfo>
-				<pubinfo>
-					<pubplace>Reston, VA</pubplace>
-					<publish>U.S. Geological Survey</publish>
-				</pubinfo>
-				<onlink>http://pubs.usgs.gov/of/2005/1401/</onlink>
-			</citeinfo>
-		</crossref>"
+
+new_crossref = "<citeinfo><origin>Peter Ruggiero</origin><origin>Meredith Kratzmann</origin><origin>Emily Himmelstoss</origin><origin>E. Robert Thieler</origin><origin>David Reid</origin><pubdate>2013</pubdate><title>National Assessment of Shoreline Change: Historical Shoreline Change along the Pacific Northwest Coast</title><serinfo><sername>Open-File Report</sername><issue>2012-1007</issue></serinfo><pubinfo><pubplace>Reston, VA</pubplace><publish>U.S. Geological Survey</publish></pubinfo><onlink>https://pubs.usgs.gov/of/2012/1007/</onlink></citeinfo>""
+
+
+tree = etree.parse(xml_file) # parse metadata using etree
+metadata_root=tree.getroot()
+# fstr = './idinfo/crossref'
+# container, tag = os.path.split(fstr)
+container = './idinfo'
+tag = 'crossref'
+elem = metadata_root.findall(container)[0]
+elem.append(etree.Element(tag)) # append new tag to container element
+elems = metadata_root.findall(fstr)
+elems[len(elems)].text = new_crossref # set new value to final (new) tag
+tree.write(xml_file)
